f0d887c936dd2d9b2d0374c03a5a4c8f33ce7034
ryota0527 <yamazaki.ryota.s3@dc.tohoku.ac.jp>
2026-08-01 10:42:15 +0900

    completion feature improved

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ mkdir -p "$HOME/todo_cli"
 #install
 cp -r ./src "$HOME/todo_cli/src"
 cp ./todo "$HOME/.local/bin/todo"
+cp ./.completion-todo "$HOME/todo_cli/.completion-todo"
 chmod +x "$HOME/.local/bin/todo"
+chmod +x "$HOME/todo_cli/.completion-todo"
 
 case ":$PATH:" in
     *":$HOME/.local/bin:"*)
@@ -18,6 +20,9 @@ case ":$PATH:" in
         ;;
 esac
 
+if ! grep -q 'source "$HOME/todo_cli/.completion-todo"' ~/.bashrc; then
+    echo 'source "$HOME/todo_cli/.completion-todo"' >> ~/.bashrc
+fi
 
 echo "Installation completed."
 echo "Run: source ~/.bashrc"
